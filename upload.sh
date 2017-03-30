2a3bcdd35a7447ffd4e2028d7d6cd4677372a915
eliza0x <me@eliza.link>
2017-03-30 20:24:23 +0900

    Update: upload script

--- upload.sh
+++ upload.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo "---------------"
 echo "START BUILDING..."
+echo "---------------"
 stack build
 stack exec site clean
 stack exec site build
@@ -8,12 +10,15 @@ tmpdir=$(mktemp -d)
 rsync -vr _site/* $tmpdir
 git checkout master
 rsync -vr $tmpdir/* .
-echo ""
+echo "---------------"
 echo "START UPLOADING..."
-echo ""
+echo "---------------"
 git add .
-git commit -m "Update"
+# コミットメッセージに時間を追加
+time=$(date)
+git commit -m "Update: $USER $time"
 git push origin master
 git checkout source
-echo ""
+echo "---------------"
 echo "SUCCESS!"
+echo "---------------"
